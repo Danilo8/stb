6a1b90a58dd649d24aa753d8da88683fd2b99a06
Danilo8 <danilodesenvolvedorweb@hotmail.com>
2018-11-09 17:28:10 -0200

    Configurando sistema de rotas

---
+++ app/Views/error/404.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="pt-BR">
+    <head>
+        <title>Página não encontrada</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1 class="text-center mt-5">
+            404. Página não encontrada!
+        </h1>
+    </body>
+</html>
